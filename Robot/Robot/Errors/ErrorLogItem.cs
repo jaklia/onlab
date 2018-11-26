0c968d07f2b2cae7ef356ef7e862ea557e467c39
jaklia <aidajakli@gmail.com>
2018-11-26 03:17:54 +0100

    error log item class added

---
+++ Robot/Robot/Errors/ErrorLogItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.Errors
+{
+    public class ErrorLogItem
+    {
+        public string Text { get; private set; }
+
+        public string Type { get; private set; }
+
+        public int Line { get; private set; }
+
+        public ErrorLogItem(string text, string type, int line)
+        {
+            Text = text;
+            Type = type;
+            Line = line;
+        }
+    }
+}
